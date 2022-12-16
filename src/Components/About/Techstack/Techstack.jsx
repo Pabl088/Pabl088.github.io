b909f786d02e3aa4e22ab865f8dd714c37a0a874
Pabl088 <pnaf88@gmail.com>
2022-12-16 15:50:34 -0300

    update

--- src/Components/About/Techstack/Techstack.jsx
+++ src/Components/About/Techstack/Techstack.jsx
@@ -7,51 +7,51 @@ function Techstack() {
     return (
         <>
             <div className={style.container}>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation1}`}>
                     <TbBrandHtml5 className={style.techicon} />
                     <span>HTML5</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation2}`}>
                     <TbBrandCss3 className={style.techicon} />
                     <span>CSS3</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation3}`}>
                     <TbBrandJavascript className={style.techicon} />
                     <span>Javascript</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation4}`}>
                     <SiTypescript style={{ fontSize: '70px', marginTop: '12px' }} className={style.techicon} />
                     <span>TypeScript</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation5}`}>
                     <SiReact className={style.techicon} />
                     <span>React.js</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation6}`}>
                     <DiNodejs className={style.techicon} />
                     <span>Node.js</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation7}`}>
                     <SiNestjs className={style.techicon} />
                     <span>Nest.js</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation8}`}>
                     <SiJava className={style.techicon} />
                     <span>Java</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation9}`}>
                     <SiSpring className={style.techicon} />
                     <span>Spring</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation10}`}>
                     <SiHibernate className={style.techicon} />
                     <span>Hibernate</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation11}`}>
                     <SiPostgresql className={style.techicon} />
                     <span>PostgreSQL</span>
                 </div>
-                <div className={style.techbox}>
+                <div className={`${style.techbox} ${style.animation12}`}>
                     <SiMongodb className={style.techicon} />
                     <span>MongoDB</span>
                 </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,9 +18,10 @@ function Home() {
         </div>
         <div className={style.secondContainer}>
             <h2>LET ME INTRODUCE MYSELF</h2>
-            <p>
-                I am passionate about learning and challenges, both individually and in groups. I consider myself proactive and responsible. Since I was a child I had an interest in technology, therefore, I was always learning all kinds of related things, different languages and/or development technologies in a self-taught way. This is why I decided to turn my life around and dedicate myself fully to what I like and am passionate about, which is this beautiful world of web and software development.
-            </p>
+            <p>I fell in love with technology since I was a child.</p>
+            <p>I am fluent in classics like Java and Javascript.</p>
+            <p>My field of interest's are building new web technologies and products, also in areas related to cybersecurity.</p>
+            <p style={{ marginBottom: 20 }}>I have a preference for backend development but I enjoy developing in all areas.</p>
         </div>
         <div className={style.contactContainer}>
             <h2>FIND ME ON</h2>
