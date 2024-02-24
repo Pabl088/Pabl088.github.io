c8c01b899d7c90a57272a033926a88088769c817
Pabl088 <pnaf88@gmail.com>
2024-02-24 16:44:54 -0300

    change to hashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.js';
 import axios from 'axios';
@@ -23,9 +23,9 @@ window.onfocus = () => {
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
